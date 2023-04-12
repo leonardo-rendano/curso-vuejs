f6964619ed0f62c175c1b5a242e3ca4aa2659d2a
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-04-12 13:26:16 -0300

    atributos

---
+++ src/components/Picture.vue
@@ -0,0 +1,15 @@
+<template>
+  <img v-bind:src="avatar" v-bind:alt="descricao">
+</template>
+
+<script>
+  export default {
+    name: 'Picture',
+    data() {
+      return {
+        avatar: '/img/avatar.png',
+        descricao: 'Imagem do avatar'
+      }
+    }
+  }
+</script>
