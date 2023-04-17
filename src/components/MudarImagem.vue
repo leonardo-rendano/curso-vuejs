fbf22999f44ea9384499fe959ae59824201b3d53
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-04-17 17:10:09 -0300

    emit

---
+++ src/components/MudarImagem.vue
@@ -0,0 +1,12 @@
+<template>
+  <button @click="$emit('mudarImagem')">
+    Trocar imagem de perfil
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'MudarImagem',
+    emits: ['mudarImagem']
+  }
+</script>
--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,14 +1,26 @@
 <template>
   <img v-bind:src="avatar" v-bind:alt="descricao">
+  <MudarImagem @mudar-imagem="trocarImagem"/>
 </template>
 
 <script>
+
+  import MudarImagem from './MudarImagem.vue'
+
   export default {
     name: 'Picture',
+    components:{
+      MudarImagem
+    },
     data() {
       return {
         avatar: '/img/avatar.png',
         descricao: 'Imagem do avatar'
+      }
+    },
+    methods: {
+      trocarImagem() {
+        this.avatar = '/img/avatar2.png'
       }
     }
   }
